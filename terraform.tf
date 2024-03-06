e26a4c4974b12921b172b40844a13884fec6c6f9
root <root@eks-test.asia-northeast3-a.c.lemon-docker.internal>
2024-03-07 04:32:41 +0900

    [modify] helm provider cluster_ca_certificate

--- terraform.tf
+++ terraform.tf
@@ -39,7 +39,7 @@ terraform {
 provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    cluster_ca_certificate = base64decode(module.eks.cluster.certificate_authority.0.data)
 #    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     token                  = data.aws_eks_cluster_auth.eks_cluster.token
   }
